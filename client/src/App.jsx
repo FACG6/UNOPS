9c0c5aedce7f3e016dac3f8fe85c4bb2e24e63ae
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-04-17 19:46:18 +0300

    render the login component to the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 import './App.css';
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <div />} />
+      <Route exact path="/login" component={() => <Login />} />
     </Switch>
   </BrowserRouter>
 );

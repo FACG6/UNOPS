ea7388858fb7dbd8add116f0c613c23856986206
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-04-17 19:52:42 +0300

    update classes names

--- client/src/components/MainSidebar/index.jsx
+++ client/src/components/MainSidebar/index.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import "./style.css";
-import { ReactComponent as Logo } from "./../../logo.svg";
+import React from 'react';
+import './style.css';
+import { ReactComponent as Logo } from '../../logo.svg';
 
 export default ({ selected }) => (
-  <aside className="main-side-bar">
-    <a href="/" className="logo-div tab">
+  <aside className="main-sidebar">
+    <a href="/" className="main-sidebar__tab">
       <Logo />
     </a>
 
-    <a href="/" className={`tickets tab ${selected === "t" ? "focus" : ""}`}>
-      <i class="fas fa-ticket-alt ticket-icon" />
-      <span className="tickets-text text">Tickets</span>
+    <a href="/" className={`main-sidebar__tab ${selected === 't' ? 'main-sidebar__focus' : ''}`}>
+      <i className="fas fa-ticket-alt ticket-icon" />
+      <span className="main-sidebar__text">Tickets</span>
     </a>
 
-    <a href="/" className={`search tab ${selected === "s" ? "focus" : ""}`}>
-      <i class="fas fa-search search-icon" />
-      <span className="search-text text">Search</span>
+    <a href="/" className={`main-sidebar__tab ${selected === 's' ? 'main-sidebar__focus' : ''}`}>
+      <i className="fas fa-search search-icon" />
+      <span className="main-sidebar__text">Search</span>
     </a>
 
-    <a href="/" className={`reports tab ${selected === "r" ? "focus" : ""}`}>
-      <i class="fas fa-chart-bar chart-icon" />
-      <span className="reports-text text">Reports</span>
+    <a href="/" className={`main-sidebar__tab ${selected === 'r' ? 'main-sidebar__focus' : ''}`}>
+      <i className="fas fa-chart-bar chart-icon" />
+      <span className="main-sidebar__text">Reports</span>
     </a>
   </aside>
 );

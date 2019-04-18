d994e66d208e59b72a9c23186500eae9a57bc7a6
alaabadra <alaabadra4@gmail.com>
2019-04-18 14:06:30 +0300

    create compnent open ticket

---
+++ client/src/components/OpenTickets/LogoTicket/index.jsx
@@ -0,0 +1,10 @@
+import React , { Component } from "react";
+export default class LogoTicket extends Component {
+    render() {
+        return (
+            <div className="symbol_logo">
+                
+            </div>
+        );
+    }
+}

66893c957f65d365e3fad97cf593f2f5e845c3c7
alaabadra <alaabadra4@gmail.com>
2019-04-18 14:33:59 +0300

    i added in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
 import MainSidebar from './components/MainSidebar';
-
+import openTicket from './components/OpenTickets/TickectSubject';
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={() => <div />} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/main-sidebar" component={() => <MainSidebar selected="tickets" />} />
+      <Route  exact path="/openTicket" component={openTicket} />
     </Switch>
   </BrowserRouter>
 );

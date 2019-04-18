fc8d170728d4f561b10227ff5b3e7d557c16a2da
Jamalat <jamalatshamallakh@gmail.com>
2019-04-18 13:42:52 +0300

    add wrappedTicket component with style Relates #27

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import WrappedTicket from './component/WrappedTicket/index'
 
 export default () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={() => <div />} />
+      <Route exact path="/" component={WrappedTicket} />
     </Switch>
   </BrowserRouter>
 );
---
+++ client/src/component/WrappedTicket/index.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom'
+import './style.css'
+class WrappedTicket extends Component {
+    state = {
+        wrappedTickets: [],
+    }
+    componentDidMount() {
+        this.setState({
+            wrappedTickets: [{
+                id: 1,
+                email: 'jamalat@gmail.com',
+                description: 'This is a sample ticket with some sample description',
+                date: 'April 9'
+                , subject: "subject"
+            },
+            {
+                id: 2,
+                email: 'jamalat@gmail.com',
+                description: 'This is a sample ticket with some sample description',
+                date: 'April 9'
+                , subject: "subject"
+            }, {
+                id: 3,
+                email: 'jamalat@gmail.com',
+                description: 'This is a sample ticket with some sample description',
+                date: 'April 9'
+                , subject: "subject"
+            }, {
+                id: 4,
+                email: 'jamalat@gmail.com',
+                description: 'This is a sample ticket with some sample description',
+                date: 'April 9'
+                , subject: "subject"
+            }]
+        })
+    }
+    render() {
+        if (this.state.wrappedTickets) {
+            return (
+                <div className="wrapped-ticket-container">
+                    <div> {this.state.wrappedTickets.map(wrappedticket =>
+                        <Route>
+                            <Link to={`/ticket/${wrappedticket.id}`} className="wrapped-ticket-container__link">
+                                <div className="wrapped-ticket-container__wrappedticket" >
+                                    <div className="wrapped-ticket-container__check-email-container"><input type="checkbox" className="wrapped-ticket-container__checkbox" />
+                                        {wrappedticket.email}
+                                    </div>
+                                    <p>{wrappedticket.subject}</p>
+                                    <p>{wrappedticket.description}</p>
+                                    <p className="wrapped-ticket-container__date"> {wrappedticket.date} </p>
+                                </div></Link>
+                        </Route>
+
+                    )}
+                    </div>
+                </div>
+            )
+
+        } else
+            return ""
+    }
+}
+
+export default WrappedTicket

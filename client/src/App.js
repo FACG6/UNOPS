983d7225d7c59b882f9758e37b1aab7664df5a36
alaabadra <alaabadra4@gmail.com>
2019-04-18 14:07:56 +0300

    create compnent open ticket

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import LogoTicket from './components/OpenTickets/LogoTicket';
+import ContentLogo from "./components/OpenTickets/ContentLogo";
+import TickectSubject from "./components/OpenTickets/TickectSubject";
+import Replies from "./components/OpenTickets/Replies";
+import TheRely from "./components/OpenTickets/TheRely";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+         < LogoTicket />
+    < ContentLogo />
+      < TickectSubject />
+     <Replies />
+     < TheRely /> 
+      </React.Fragment>
+        
+      
+   
+
     );
   }
 }

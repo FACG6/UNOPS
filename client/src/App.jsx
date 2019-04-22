911d7dd3bb1df6bb60f3f061b7b595ccec8fe1fd
Jamalat <jamalatshamallakh@gmail.com>
2019-04-22 11:37:18 +0300

    change the ticketsidebar to function instead of class component Relates #24

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
 import NewTicket from './components/NewTicket';
 import MainSidebar from './components/MainSidebar';
+import TicketsSidebar from './components/TicketsSidebar';
 
-export default () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" />
-      <Route exact path="/new-ticket" component={NewTicket} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/main-sidebar" component={() => <MainSidebar selected="tickets" />} />
-    </Switch>
-  </BrowserRouter>
-);
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={
+            () => (
+              <Fragment>
+                <MainSidebar />
+                <TicketsSidebar selected="all" />
+              </Fragment>
+            )
+          }
+          />
+          <Route exact path="/new-ticket" component={NewTicket} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/main-sidebar" component={() => <MainSidebar selected="tickets" />} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+export default App;
--- client/src/components/TicketsSidebar/index.jsx
+++ client/src/components/TicketsSidebar/index.jsx
@@ -1,42 +1,37 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './style.css';
 
-class TicketsSidebar extends Component {
-  render() {
-    return (
-      <div className="ticket-sidebar">
-        <button className="ticket-sidebar__new-ticket">
-          <i className="fas fa-plus" />
-          {'          '}
-          Ticket
-        </button>
-        <div className="ticket-sidebar__links-container">
-          <Router>
-            <div className="ticket-sidebar__links">
-              <Link to="/tickets/all-ticket" className="ticket-sidebar__all-link">
-                All
-                {' '}
-              </Link>
-              <i className="far fa-circle" />
-            </div>
-            <div className="ticket-sidebar__links">
-              <Link to="/tickets/my-ticket" className="ticket-sidebar__my-link">My</Link>
-              <i className="far fa-circle" />
-            </div>
-            <div className="ticket-sidebar__links">
-              <Link to="/tickets/drafts-ticket" className="ticket-sidebar__drafts-link">Drafts</Link>
-              <i className="far fa-circle" />
-            </div>
-            <div className="ticket-sidebar__links">
-              <Link to="/tickets/trash-ticket" className="ticket-sidebar__trash-link">Trash</Link>
-              <i className="far fa-circle" />
-            </div>
-          </Router>
+const TicketsSidebar = ({ selected }) => (
+  <div className="ticket-sidebar">
+    <button className="ticket-sidebar__new-ticket">
+      <i className="fas fa-plus" />
+      {'          '}
+      Ticket
+    </button>
+    <div className="ticket-sidebar__links-container">
+      <Router>
+        <div className={`ticket-sidebar__links  ${selected === 'all' ? 'selected-tab' : ''}`}>
+          <Link to="/tickets/all-ticket" className={`ticket-sidebar__all-link ${selected === 'all' ? 'ticket-sidebar__all-link-selected' : ''}`}>
+            All
+            {' '}
+          </Link>
+          <i className={`far fa-circle  ${selected === 'all' ? 'fa-circle-selected' : ''}`} />
         </div>
-
-      </div>
-    );
-  }
-}
+        <div className={`ticket-sidebar__links  ${selected === 'my' ? 'selected-tab' : ''}`}>
+          <Link to="/tickets/my-ticket" className={`ticket-sidebar__my-link ${selected === 'my' ? 'selected-tab' : ''}`}>My</Link>
+          <i className={`far fa-circle  ${selected === 'my' ? 'fa-circle-selected' : ''}`} />
+        </div>
+        <div className={`ticket-sidebar__links  ${selected === 'drafts' ? 'selected-tab' : ''}`}>
+          <Link to="/tickets/drafts-ticket" className={`ticket-sidebar__drafts-link ${selected === 'drafts' ? 'selected-tab' : ''}`}>Drafts</Link>
+          <i className={`far fa-circle  ${selected === 'drafts' ? 'fa-circle-selected' : ''}`} />
+        </div>
+        <div className={`ticket-sidebar__links  ${selected === 'trash' ? 'selected-tab' : ''}`}>
+          <Link to="/tickets/trash-ticket" className={`ticket-sidebar__trash-link ${selected === 'trash' ? 'selected-tab' : ''}`}>Trash</Link>
+          <i className={`far fa-circle  ${selected === 'trash' ? 'fa-circle-selected' : ''}`} />
+        </div>
+      </Router>
+    </div>
+  </div>
+);
 export default TicketsSidebar;

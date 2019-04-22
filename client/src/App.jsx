786fcfe4baeaf6b4faffb6871e643c8b6ceefa08
Jamalat <jamalatshamallakh@gmail.com>
2019-04-22 14:59:30 +0300

    change replies component to stateless component Relates #29 #31

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import './App.css';
@@ -6,14 +6,47 @@ import NewTicket from './components/NewTicket';
 import MainSidebar from './components/MainSidebar';
 import Replies from './components/Replies/index';
 
-export default () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Replies} />
-      <Route exact path="/new-ticket" component={NewTicket} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/main-sidebar" component={() => <MainSidebar selected="tickets" />} />
-      <Route exact path="/ticket" component={() => <MainSidebar selected="tickets" />} />
-    </Switch>
-  </BrowserRouter>
-);
+class App extends Component {
+  state = {
+    replies: [],
+  }
+
+  componentDidMount() {
+    this.setState({
+      replies: [
+        {
+          id: 1,
+          email: 'jamalat@getsMaxListeners.com',
+          message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, ',
+        },
+        {
+          id: 2,
+          email: 'jamalat@getMadxListeners.com',
+          message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, ',
+        },
+        {
+          id: 3,
+          email: 'jamalat@getMaxListeners.com',
+          message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, ',
+        },
+      ],
+    });
+  }
+
+  render() {
+    if (this.state.replies) {
+      return (
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" />
+            <Route exact path="/new-ticket" component={NewTicket} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/main-sidebar" component={() => <MainSidebar selected="tickets" />} />
+            <Route exact path="/ticket" component={() => <Replies replies={this.state.replies} />} />
+          </Switch>
+        </BrowserRouter>
+      );
+    } return <h2>There are no Replies yet.</h2>;
+  }
+}
+export default App;
--- client/src/components/Replies/index.jsx
+++ client/src/components/Replies/index.jsx
@@ -2,43 +2,13 @@ import React, { Component } from 'react';
 import Conversation from '../Conversation';
 import './style.css';
 
-class Replies extends Component {
-    state = {
-      replies: [],
-    }
+const Replies = props => (
+  <div className="replies-component">
+    <p className="replies-component__replies-text">Replies :</p>
+    <div>
+      {props.replies.map(reply => <Conversation key={reply.id} reply={reply} />)}
 
-    componentDidMount() {
-      this.setState({
-        replies: [
-          {
-            id: 1,
-            email: 'jamalat@getsMaxListeners.com',
-            message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, ',
-          },
-          {
-            id: 2,
-            email: 'jamalat@getMadxListeners.com',
-            message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, ',
-          },
-          {
-            id: 3,
-            email: 'jamalat@getMaxListeners.com',
-            message: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, ',
-          },
-        ],
-      });
-    }
-
-    render() {
-      return (
-        <div className="replies-component">
-          <p className="replies-component__replies-text">Replies :</p>
-          <div>
-            {this.state.replies.map(reply => <Conversation key={reply.id} reply={reply} />)}
-
-          </div>
-        </div>
-      );
-    }
-}
+    </div>
+  </div>
+);
 export default Replies;

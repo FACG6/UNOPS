d9ca614826851c80e5fc4fc7b15395ebd40a7ff6
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-04-25 16:07:20 +0300

    update the reply component

--- client/src/components/parts/Replies/index.jsx
+++ client/src/components/parts/Replies/index.jsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ReplyCard from '../ReplyCard';
-import './style.css';
+import React from "react";
+import PropTypes from "prop-types";
+import ReplyCard from "../ReplyCard";
+import "./style.css";
 
 export default function Replies({ tickets }) {
   return (
     <section className="replies-section">
       <div className="replies-section__text">Replies:</div>
       {tickets.map(ticket => (
-        <ReplyCard
-          from={ticket.from}
-          cc={ticket.cc}
-          bcc={ticket.bcc}
-          user={ticket.user}
-          reply={ticket.reply}
-        />
+        <ReplyCard {...ticket} />
       ))}
     </section>
   );
 }
 
 Replies.propTypes = {
-  tickets: PropTypes.instanceOf(Array).isRequired,
+  tickets: PropTypes.instanceOf(Array).isRequired
 };
--- client/src/components/parts/Reply/index.jsx
+++ client/src/components/parts/Reply/index.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './style.css';
+import React, { Component } from "react";
+import EmailCopy from "./../EmailCopy";
+import PropTypes from "prop-types";
+import "./style.css";
 
 export default class Reply extends Component {
   state = {
@@ -8,21 +9,19 @@ export default class Reply extends Component {
     bcc: [],
     addCc: false,
     addBcc: false,
-    reply: '',
+    reply: ""
   };
   toggleAdd = target => {
-    target = target === 'cc' ? 'addCc' : 'addBcc';
+    target = target === "cc" ? "addCc" : "addBcc";
     this.setState({ [target]: !this.state[target] });
   };
-  addNew = event => {
-    const target = event.target.parentNode.classList.contains('reply-section__copies-cc')
-      ? 'cc'
-      : 'bcc';
+  addNew = (event, target) => {
     if (event.charCode == 13) {
       if (!event.target.value) this.toggleAdd(target);
       else
-        this.setState({ [target]: [...this.state[target], event.target.value] }, () =>
-          this.toggleAdd(target),
+        this.setState(
+          { [target]: [...this.state[target], event.target.value] },
+          () => this.toggleAdd(target)
         );
     }
   };
@@ -34,64 +33,20 @@ export default class Reply extends Component {
         <div className="reply-section__text">Reply:</div>
         <div className="reply-section__reply-form">
           <div className="reply-section__copies">
-            {addCc ? (
-              <div className="reply-section__copies-cc">
-                CC:
-                <input
-                  type="email"
-                  className="reply-section__copies-input reply-section__input"
-                  onKeyPress={this.addNew}
-                />
-                {cc.length ? (
-                  cc.map(email => <span className="reply-section__copies-email">{email}</span>)
-                ) : (
-                  <></>
-                )}
-                <i className="far fa-plus-square reply-section__plus-icon" />
-              </div>
-            ) : (
-              <div className="reply-section__copies-cc">
-                CC:
-                {cc.length ? (
-                  cc.map(email => <span className="reply-section__copies-email">{email}</span>)
-                ) : (
-                  <></>
-                )}
-                <i
-                  className="far fa-plus-square reply-section__plus-icon"
-                  onClick={() => this.toggleAdd('cc')}
-                />
-              </div>
-            )}
-            {addBcc ? (
-              <div className="reply-section__copies-bcc">
-                BCC:
-                <input
-                  type="email"
-                  className="reply-section__copies-input reply-section__input"
-                  onKeyPress={this.addNew}
-                />
-                {bcc.length ? (
-                  bcc.map(email => <span className="reply-section__copies-email">{email}</span>)
-                ) : (
-                  <></>
-                )}
-                <i className="far fa-plus-square reply-section__plus-icon" />
-              </div>
-            ) : (
-              <div className="reply-section__copies-bcc">
-                BCC:
-                {bcc.length ? (
-                  bcc.map(email => <span className="reply-section__copies-email">{email}</span>)
-                ) : (
-                  <></>
-                )}
-                <i
-                  className="far fa-plus-square reply-section__plus-icon"
-                  onClick={() => this.toggleAdd('bcc')}
-                />
-              </div>
-            )}
+            <EmailCopy
+              add={addCc}
+              type="cc"
+              copy={cc}
+              addNew={this.addNew}
+              toggleAdd={this.toggleAdd}
+            />
+            <EmailCopy
+              add={addBcc}
+              type="bcc"
+              copy={bcc}
+              addNew={this.addNew}
+              toggleAdd={this.toggleAdd}
+            />
           </div>
           <textarea className="reply-section__reply-field" />
           <div className="reply-section__submit">Reply</div>

cfc816655d9829cb163b1d60592d56daa1698503
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-04-25 16:11:31 +0300

    update the carbon copy sections to use the email copy component

--- client/src/components/parts/NewTicket/index.jsx
+++ client/src/components/parts/NewTicket/index.jsx
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
-import './style.css';
+import React, { Component } from "react";
+import EmailCopy from "./../EmailCopy";
+import "./style.css";
 
 export default class NewTicket extends Component {
   state = {
-    subject: '',
-    to: '',
-    cc: '',
-    bcc: '',
-    body: '',
+    subject: "",
+    to: "",
+    cc: "",
+    bcc: "",
+    body: "",
     addCc: false,
     addBcc: false,
-    sendAsEmail: false,
+    sendAsEmail: false
   };
 
   toggleAdd = target => {
-    target = target === 'cc' ? 'addCc' : 'addBcc';
+    target = target === "cc" ? "addCc" : "addBcc";
     this.setState({ [target]: !this.state[target] });
   };
 
-  addNew = event => {
-    const target = event.target.parentNode.classList.contains('new-ticket__copies-cc')
-      ? 'cc'
-      : 'bcc';
+  addNew = (event, target) => {
     if (event.charCode == 13) {
       if (!event.target.value) this.toggleAdd(target);
       else
-        this.setState({ [target]: [...this.state[target], event.target.value] }, () =>
-          this.toggleAdd(target),
+        this.setState(
+          { [target]: [...this.state[target], event.target.value] },
+          () => this.toggleAdd(target)
         );
     }
   };
@@ -35,7 +34,9 @@ export default class NewTicket extends Component {
     const { cc, bcc, addCc, addBcc } = this.state;
     return (
       <section className="new-ticket">
-        <div className="new-ticket__text new-ticket__text--strong">New Ticket</div>
+        <div className="new-ticket__text new-ticket__text--strong">
+          New Ticket
+        </div>
         <form className="new-ticket__form">
           <div className="new-ticket__label">
             <span className="new-ticket__text">
@@ -56,70 +57,31 @@ export default class NewTicket extends Component {
             />
           </div>
           <div className="new-ticket__copies-div">
-            {addCc ? (
-              <div className="new-ticket__copies-cc">
-                CC:
-                <input
-                  type="email"
-                  className="new-ticket__copies-input new-ticket__input"
-                  onKeyPress={this.addNew}
-                  placeholder="Enter CC"
-                />
-                {cc.length ? (
-                  cc.map(email => <span className="new-ticket__copies-email">{email}</span>)
-                ) : (
-                  <></>
-                )}
-                <i className="far fa-plus-square new-ticket__plus-icon" />
-              </div>
-            ) : (
-              <div className="new-ticket__copies-cc">
-                CC:
-                {cc.length ? (
-                  cc.map(email => <span className="new-ticket__copies-email">{email}</span>)
-                ) : (
-                  <></>
-                )}
-                <i
-                  className="far fa-plus-square new-ticket__plus-icon"
-                  onClick={() => this.toggleAdd('cc')}
-                />
-              </div>
-            )}
-            {addBcc ? (
-              <div className="new-ticket__copies-bcc">
-                BCC:
-                <input
-                  type="email"
-                  className="new-ticket__copies-input new-ticket__input"
-                  onKeyPress={this.addNew}
-                  placeholder="Enter BCC"
-                />
-                {bcc.length ? (
-                  bcc.map(email => <span className="new-ticket__copies-email">{email}</span>)
-                ) : (
-                  <></>
-                )}
-                <i className="far fa-plus-square new-ticket__plus-icon" />
-              </div>
-            ) : (
-              <div className="new-ticket__copies-bcc">
-                BCC:
-                {bcc.length ? (
-                  bcc.map(email => <span className="new-ticket__copies-email">{email}</span>)
-                ) : (
-                  <></>
-                )}
-                <i
-                  className="far fa-plus-square new-ticket__plus-icon"
-                  onClick={() => this.toggleAdd('bcc')}
-                />
-              </div>
-            )}
+            <EmailCopy
+              add={addCc}
+              type="cc"
+              copy={cc}
+              addNew={this.addNew}
+              toggleAdd={this.toggleAdd}
+            />
+            <EmailCopy
+              add={addBcc}
+              type="bcc"
+              copy={bcc}
+              addNew={this.addNew}
+              toggleAdd={this.toggleAdd}
+            />
           </div>
-          <textarea className="new-ticket__message-field" placeholder="Enter your message" />
+          <textarea
+            className="new-ticket__message-field"
+            placeholder="Enter your message"
+          />
           <label htmlFor="checkbox" className="new-ticket__send-as-mail">
-            <input type="checkbox" id="checkbox" className="new-ticket__checkbox" />
+            <input
+              type="checkbox"
+              id="checkbox"
+              className="new-ticket__checkbox"
+            />
             <span className="new-ticket__text">Send the ticket as email.</span>
           </label>
           <div className="new-ticket__submit">Create Ticket</div>

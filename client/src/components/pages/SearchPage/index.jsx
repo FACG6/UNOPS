6bfdefe764c7f26bfb8c612d2cdee1cfbb6d22b4
Ahmed I. Abdellatif <ahmedisam9922@gmail.com>
2019-04-25 16:51:41 +0300

    update variables names

--- client/src/components/pages/SearchPage/index.jsx
+++ client/src/components/pages/SearchPage/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import SearchSidebar from '../../parts/SearchSidebar';
-import MainSidebar from '../../parts/MainSidebar';
-import Navbar from '../../parts/Navbar';
-import WrappedTicket from '../../parts/WrappedTicket';
-import './style.css';
+import React from "react";
+import PropTypes from "prop-types";
+import SearchSidebar from "../../parts/SearchSidebar";
+import MainSidebar from "../../parts/MainSidebar";
+import Navbar from "../../parts/Navbar";
+import WrappedTicket from "../../parts/WrappedTicket";
+import "./style.css";
 
 export default function SearchPage({
-  searchResults, tickets, searchAction, pending, closed,
+  searchResults,
+  tickets,
+  searchAction,
+  pending,
+  closed
 }) {
   if (searchResults) {
     return (
@@ -27,7 +31,12 @@ export default function SearchPage({
   }
   return (
     <>
-      <Navbar selected="pending" pending={pending} closed={closed} currentCategory="all" />
+      <Navbar
+        selected="pending"
+        pending={pending}
+        closed={closed}
+        currentCategory="all-tickets"
+      />
       <MainSidebar selected="search" />
       <SearchSidebar />
       <main className="search-page__tickets-section">
@@ -44,12 +53,12 @@ SearchPage.propTypes = {
   tickets: PropTypes.instanceOf(Array),
   searchAction: PropTypes.instanceOf(Function).isRequired,
   pending: PropTypes.number,
-  closed: PropTypes.number,
+  closed: PropTypes.number
 };
 
 SearchPage.defaultProps = {
   searchResults: undefined,
   tickets: undefined,
   pending: 0,
-  closed: 0,
+  closed: 0
 };
--- client/src/components/pages/TicketsPage/index.jsx
+++ client/src/components/pages/TicketsPage/index.jsx
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import MainSidebar from '../../parts/MainSidebar';
-import TicketsSidebar from '../../parts/TicketsSidebar';
-import Navbar from '../../parts/Navbar';
-import WrappedTicket from '../../parts/WrappedTicket';
-import './style.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import MainSidebar from "../../parts/MainSidebar";
+import TicketsSidebar from "../../parts/TicketsSidebar";
+import Navbar from "../../parts/Navbar";
+import WrappedTicket from "../../parts/WrappedTicket";
+import "./style.css";
 
 export default class extends Component {
   state = {
     tickets: this.props.tickets || {
-      all: {
+      allTickets: {
         pending: [],
-        closed: [],
+        closed: []
       },
-      my: {
+      myTickets: {
         pending: [],
-        closed: [],
+        closed: []
       },
       drafts: [],
-      trash: [],
+      trash: []
     },
-    currentCategory: this.props.match.params.category || 'all',
-    currentStatus: this.props.match.params.status || 'pending',
+    currentCategory: this.props.match.params.category || "all-tickets",
+    currentStatus: this.props.match.params.status || "pending",
     allChecked: false,
-    markAs: null,
+    markAs: null
   };
 
   componentWillReceiveProps(nextProps) {
     this.setState({
       tickets: nextProps.tickets,
       currentCategory: nextProps.match.params.category,
-      currentStatus: nextProps.match.params.status,
+      currentStatus: nextProps.match.params.status
     });
   }
 
@@ -39,19 +39,27 @@ export default class extends Component {
   };
 
   render() {
-    const { currentCategory, currentStatus, allChecked, markAs, tickets } = this.state;
+    const {
+      currentCategory,
+      currentStatus,
+      allChecked,
+      markAs,
+      tickets
+    } = this.state;
 
-    if (!(currentCategory === 'trash' || currentCategory === 'drafts')) {
-      var pendingLength = tickets[currentCategory].pending.length,
-        closedLength = tickets[currentCategory].closed.length,
-        allLength = tickets.all.pending.length + tickets.all.closed.length,
-        myLength = tickets.my.pending.length + tickets.my.closed.length,
+    if (!(currentCategory === "trash" || currentCategory === "drafts")) {
+      var pendingTicketsCount = tickets[currentCategory].pending.length,
+        closedTicketsCount = tickets[currentCategory].closed.length,
+        allTicketsCound =
+          tickets.all.pending.length + tickets.all.closed.length,
+        myTicketsCount = tickets.my.pending.length + tickets.my.closed.length,
         ticketsToRender = tickets[currentCategory][currentStatus];
     } else {
-      var pendingLength = 'null',
-        closedLength = 'null',
-        allLength = tickets.all.pending.length + tickets.all.closed.length,
-        myLength = tickets.my.pending.length + tickets.my.closed.length,
+      var pendingTicketsCount = null,
+        closedTicketsCount = null,
+        allTicketsCound =
+          tickets.all.pending.length + tickets.all.closed.length,
+        myTicketsCount = tickets.my.pending.length + tickets.my.closed.length,
         ticketsToRender = tickets[currentCategory][currentStatus];
     }
 
@@ -60,17 +68,17 @@ export default class extends Component {
         <MainSidebar selected="tickets" />
         <TicketsSidebar
           selected={currentCategory}
-          all={allLength}
-          my={myLength}
+          allTickets={allTicketsCound}
+          myTickets={myTicketsCount}
           trash={tickets.trash.length}
           drafts={tickets.drafts.length}
         />
-        {pendingLength !== 'null' ? (
+        {!pendingTicketsCount ? (
           <Navbar
             selected={currentStatus}
             currentCategory={currentCategory}
-            pending={pendingLength}
-            closed={closedLength}
+            pending={pendingTicketsCount}
+            closed={closedTicketsCount}
           />
         ) : (
           <Navbar
@@ -81,7 +89,10 @@ export default class extends Component {
         )}
         <main className="tickets-page">
           <div className="tickets-page__header">
-            <span className="tickets-page__select-all" onClick={this.toggleAllChecked}>
+            <span
+              className="tickets-page__select-all"
+              onClick={this.toggleAllChecked}
+            >
               Select all
             </span>
             <select name="markAs" id="markAs" className="tickets-page__mark-as">

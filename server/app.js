bebc1551c2da8eab8519275aaa4d77f856ae8b57
Anies <a2nies2012@gmail.com>
2019-04-28 11:41:39 +0300

    routersHandling

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { join } = require('path');
 
 const app = express();
 const cookieParser = require('cookie-parser');
+const router = require('./controller/index');
 
 app.set('port', process.env.PORT || 7425);
 app.set('host', process.env.hostname || 'localhost');
@@ -9,5 +11,13 @@ app.set('host', process.env.hostname || 'localhost');
 app.use(cookieParser);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(join(__dirname, '..', 'client', 'build')));
+
+app.use('/api/v1', router);
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 module.exports = app;
